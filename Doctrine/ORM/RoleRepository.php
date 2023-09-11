20e71ad44b98b01a4979ba061caeffa8832f55dd
owl-app <owl@pawelkeska.eu>
2023-09-11 08:47:04 +0000

    Fixes psalm

--- Doctrine/ORM/RoleRepository.php
+++ Doctrine/ORM/RoleRepository.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Owl\Bundle\RbacBundle\Doctrine\ORM;
 
+use Owl\Component\Rbac\Model\RoleInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Owl\Component\Rbac\Repository\RoleRepositoryInterface;
 
+/**
+ * @template T of RoleInterface
+ *
+ * @implements RoleRepositoryInterface<T>
+ */
 class RoleRepository extends EntityRepository implements RoleRepositoryInterface
 {
 }
